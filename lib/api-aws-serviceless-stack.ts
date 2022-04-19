fe737a115806e2e124f28f18757e9f87f883a661
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-19 19:28:14 +0800

    install dependencies
    -lambda
    -apigate way
    -dynamdb

--- lib/api-aws-serviceless-stack.ts
+++ lib/api-aws-serviceless-stack.ts
@@ -1,7 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 export class ApiAwsServicelessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
