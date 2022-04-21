97674930e1fd309c2bb2f8a726d9d8d2c206c63e
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-21 10:02:07 +0800

    add another stack

--- lib/api-aws-serviceless-stack.ts
+++ lib/api-aws-serviceless-stack.ts
@@ -3,15 +3,10 @@ import { Construct } from 'constructs';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Duration } from '@aws-cdk/core';
 export class ApiAwsServicelessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiAwsServicelessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
