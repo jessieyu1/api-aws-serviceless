d5dd6a53b15aa5fc1a8b3c5a14d75b7dc78da1ab
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-22 11:16:49 +0800

    lambda-node-js

--- lib/api-aws-serviceless-stack.ts
+++ lib/api-aws-serviceless-stack.ts
@@ -4,6 +4,7 @@ import {Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda'
 import {join} from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 import { GenericTable } from './GenericTable';
+import {NodejsFunction} from'aws-cdk-lib/aws-lambda-nodejs';
 export class ApiAwsServicelessStack extends Stack {
   
   private api = new RestApi(this, 'ApiAwsServiceless');
@@ -17,6 +18,11 @@ export class ApiAwsServicelessStack extends Stack {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main'
+    });
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
+      handler: 'handler'
     });
 
     // hello api lambda integration:
