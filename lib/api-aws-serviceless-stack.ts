1174d952cac2fff2fd3276b8b8a37819441bb71a
Jessie Yu <61171021+jessieyu1@users.noreply.github.com>
2022-04-22 12:09:41 +0800

    aws sdk

--- lib/api-aws-serviceless-stack.ts
+++ lib/api-aws-serviceless-stack.ts
@@ -1,10 +1,10 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda'
 import {join} from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 import { GenericTable } from './GenericTable';
 import {NodejsFunction} from'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 export class ApiAwsServicelessStack extends Stack {
   
   private api = new RestApi(this, 'ApiAwsServiceless');
@@ -14,20 +14,18 @@ export class ApiAwsServicelessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const HelloLambda = new LambdaFunction(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      handler: 'hello.main'
-    });
-
     const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler'
     });
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*');
+    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
     // hello api lambda integration:
 
-    const helloLambdaIntegration = new LambdaIntegration(HelloLambda);
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
     const helloLambdaResource = this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,8 +1,14 @@
 import { v4 } from 'uuid';
+import { S3 } from 'aws-sdk';
+
+const s3Client = new S3();  
+
 async function handler(event: any, context:any){
+    const buckets = await s3Client.listBuckets().promise();
+    console.log('got an event:', event);
     return{
         statusCode: 200,
-        body: "Hello World!" + v4()
+        body: "Here are your buckets: " + JSON.stringify(buckets.Buckets)
     }
 }
 
